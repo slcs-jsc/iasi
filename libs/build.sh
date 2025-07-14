bcd24534f35ec3035faa2e95c16ebd91695ea4f2
Lars Hoffmann <l.hoffmann@fz-juelich.de>
2025-07-14 22:40:43 +0200

    Update library build script.

--- libs/build.sh
+++ libs/build.sh
@@ -1,24 +1,5 @@
 #! /bin/bash
 
-# ----------------------------------------------------------------------
-function infomsg {
-# ----------------------------------------------------------------------
-    
-    echo
-    echo "============================================================"
-    echo "Compile: $1"
-    echo "============================================================"
-    echo
-}
-
-# ----------------------------------------------------------------------
-# Main...
-# ----------------------------------------------------------------------
-
-
-# Set number of cores...
-THREADS=$(cat /proc/cpuinfo | grep processor | wc -l)
-
 # Get absolute path...
 target=$(mkdir -p build && cd build && pwd)
 
@@ -27,28 +8,25 @@ mkdir -p $target/src $target/bin $target/lib $target/man/man1 \
     && cp *tar.gz $target/src \
     && cd $target/src \
     && for f in $(ls *tar.gz) ; do tar xvzf $f ; done \
-    || exit
+	|| exit
 
 # GSL...
-dir=gsl-2.5
-infomsg $dir
+dir=gsl-2.7.1
 cd $target/src/$dir \
     && ./configure --prefix=$target \
-    && make -j$THREADS && make check && make install && make clean \
-    || exit
+    && make -j && make check && make install && make clean \
+	|| exit
 
 # netCDF...
 dir=netcdf-c-4.6.2
-infomsg $dir
 cd $target/src/$dir \
     && ./configure --prefix=$target --enable-c-only --disable-dap --disable-netcdf-4\
-    && make -j$THREADS && make check && make install \
-    || exit
+    && make -j && make check && make install \
+	|| exit
 
 # coda
 dir=coda-2.20
-infomsg $dir
 cd $target/src/$dir \
     && ./configure --prefix=$target\
-    && make -j$THREADS && make check && make install \
-    || exit
+    && make -j && make check && make install \
+	|| exit
