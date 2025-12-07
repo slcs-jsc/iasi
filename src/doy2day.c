0804d5f3b6db57002864833878340b82dacc1524
Lars Hoffmann <l.hoffmann@fz-juelich.de>
2025-12-07 09:40:51 +0100

    Added day2doy, doy2day, jsec2time and time2jsec tools.

---
+++ src/doy2day.c
@@ -0,0 +1,47 @@
+/*
+  This file is part of the AIRS Code Collection.
+  
+  the AIRS Code Collections is free software: you can redistribute it
+  and/or modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation, either version 3 of
+  the License, or (at your option) any later version.
+  
+  The AIRS Code Collection is distributed in the hope that it will be
+  useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with the AIRS Code Collection. If not, see
+  <http://www.gnu.org/licenses/>.
+  
+  Copyright (C) 2019-2025 Forschungszentrum Juelich GmbH
+*/
+
+/*! 
+  \file
+  Convert day of year to date.
+*/
+
+#include "jurassic.h"
+
+int main(
+  int argc,
+  char *argv[]) {
+
+  int day, mon;
+
+  /* Check arguments... */
+  if (argc < 3)
+    ERRMSG("Give parameters: <year> <doy>");
+
+  /* Read arguments... */
+  const int year = atoi(argv[1]);
+  const int doy = atoi(argv[2]);
+
+  /* Convert... */
+  doy2day(year, doy, &mon, &day);
+  printf("%d %d %d\n", year, mon, day);
+
+  return EXIT_SUCCESS;
+}
