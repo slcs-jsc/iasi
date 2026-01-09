3ed2780d065a14458c07624e7b8ece4a53ab7869
Lars Hoffmann <l.hoffmann@fz-juelich.de>
2026-01-09 17:30:51 +0100

    Update library build script.

--- libs/build.sh
+++ libs/build.sh
@@ -1,15 +1,14 @@
 #! /bin/bash
 
 # Setup...
-rm -rf build
-target=$(mkdir -p build && cd build && pwd)
+target=$(rm -rf build && mkdir -p build && cd build && pwd)
 
 # Prepare directories...
 mkdir -p $target/src $target/bin $target/lib $target/man/man1 \
     && cp *tar.bz2 $target/src \
     && cd $target/src \
-    && for f in $(ls *tar.bz2) ; do tar xvjf $f ; done \
-    || exit
+    && for f in *.tar.bz2 ; do tar xvjf $f ; done \
+	|| exit
 
 # GSL...
 dir=gsl-2.7.1
